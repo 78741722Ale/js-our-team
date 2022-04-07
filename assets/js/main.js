f9c5cbe11ac1e702a6e56e63e28a17bb80e46334
78741722Ale <pecorillaalessandro@gmail.com>
2022-04-07 14:55:14 +0200

    Initial Commit

---
+++ assets/js/main.js
@@ -0,0 +1,8 @@
+/* 
+Viene fornito un file js in cui è definito un array di oggetti che rappresentano 
+i membri del team. Ogni membro ha le informazioni necessarie per stampare le relative 
+informazioni: Nome, Ruolo e Foto.
+*/
+
+// Milestone 1
+/* stampare su console le informazioni di nome, ruolo e la stringa della foto */
